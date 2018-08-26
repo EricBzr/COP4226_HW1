4c779c30181606c7670944356c50d1c36af9caf0
Mehdi Hajikhani <mhaji007@fiu.edu>
2018-08-26 08:41:51 -0700

    Simple button creation Note: A console application does not automatically add a reference to System.Windows.Forms.dll. Right-click your project in Solution Explorer and select Add reference, and then find System.Windows.Forms and add it.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MyFirstButton
+    {
+        static void Main(string[] args)
+        {
+            Form form  = new MyFirstForm();
+            Application.Run(form);
+
+
+        }
+    }
+    class MyFirstForm : Form
+    {
+        public MyFirstForm()
+        {
+            this.Text = "Hello, FinWorms!";
+            Button button = new Button();
+            button.Text = "Click Me!";
+            this.Controls.Add(button);
+
+        }
+
+    }
+}

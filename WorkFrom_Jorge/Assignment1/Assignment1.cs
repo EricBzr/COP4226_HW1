ebf65db8998c089d3ca23fec3b723763bba843c0
Jorge Costafreda <jorge.costafreda@yahoo.com>
2018-08-28 21:33:36 -0400

    Not complete, will work on some more before due date.

---
+++ WorkFrom_Jorge/Assignment1/Assignment1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+class Assignment1
+{
+    static void Main(string[] args)
+    {
+        String DEFAULT_TITLE = "Windows Forms!";
+        String NAME = "Jorge";
+        MyForm f = new MyForm(DEFAULT_TITLE, NAME);
+
+        Application.Run(f);
+    }
+
+}
+
+
+public class MyForm : Form
+{
+    String yourName;
+
+    public MyForm(String title, String yourName)
+    {
+        setTitle(title);
+        this.Size = new Size(512, 256);
+        this.yourName = yourName;
+
+        Panel panel = new Panel();
+        Color colorCyan = Color.FromName("DarkSlateGray");
+        panel.BackColor = colorCyan;
+        panel.Dock = DockStyle.Bottom;
+
+
+
+        Label label = new Label();
+        label.Text = yourName;
+        panel.Controls.Add(label);
+
+        this.Controls.Add(panel);
+        //panel.BackColor = new System.Drawing.Color()
+
+    }
+
+    private void setTitle(String title)
+    {
+        this.Text = title;
+    }
+
+}
+
